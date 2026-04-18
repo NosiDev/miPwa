08346cade7fef355e4e9e19fdf6d69b6dcde5811
NosiDev <devtobi43@gmail.com>
2026-04-17 21:28:59 -0300

    ..

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = "scout-pwa-v2";
+const CACHE_NAME = "scout-pwa-v3";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
+  "/",
+  "/index.html",
+  "/manifest.json",
   "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
 ];
 
 // INSTALL
 self.addEventListener("install", (event) => {
+
+  self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -34,21 +36,16 @@ self.addEventListener("activate", (event) => {
   );
 
   self.clients.claim();
+
 });
 
-// MENSAJE UPDATE
-self.addEventListener("message", (event) => {
-  if (event.data === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-// FETCH
+// FETCH (Network first)
 self.addEventListener("fetch", (event) => {
 
   if (event.request.method !== "GET") return;
 
   event.respondWith(
+
     fetch(event.request)
       .then((response) => {
 
@@ -64,10 +61,13 @@ self.addEventListener("fetch", (event) => {
 
       })
       .catch(() => {
+
         return caches.match(event.request).then((response) => {
-          return response || caches.match("./index.html");
+          return response || caches.match("/index.html");
         });
+
       })
+
   );
 
 });
